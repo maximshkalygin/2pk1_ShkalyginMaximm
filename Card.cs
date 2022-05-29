bf236f5d31ccb1e08e4b664a3ab5194747ad8317
maximshkalygin <91649154+maximshkalygin@users.noreply.github.com>
2022-05-29 21:37:05 +0300

    Add files via upload

---
+++ Card.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shkalygin
+{
+    internal class Card
+    {
+        static int ExpaierdCards = 0;
+        static  int Count = 0;
+        private int number;
+        private string clientFIO;
+        private DateTime validity;
+        private int secureCode;
+        private string type;
+        public int Number { get { return number; } set {if (value.ToString().Length==20) number = value; } } 
+        public string ClientFIO { get { return clientFIO; } set {if (!string.IsNullOrEmpty(value)) clientFIO = value; } }
+        public DateTime Validity { get => validity; set {
+                if (value >= DateTime.Parse("01.01.2022") && value <= DateTime.Parse(".01.01.2026")) 
+                {
+                    validity = value; 
+                    if (value.Date==DateTime.Now.Date)
+                        ExpaierdCards++;
+                } 
+            }
+        }
+        public int SecureCode { get => secureCode; set { if (value.ToString().Length==3) secureCode = value; } }
+        public string Type { get => type; set {if (!string.IsNullOrEmpty(value)) type = value; } }
+
+        public string GetInfo()
+        {
+            string info = $"Ваш номер карты {number}, Ваше ФИО {clientFIO},Срок действия {Validity}, Код безопасности {SecureCode},Карта {Type}";
+            return info;
+        }
+        public static int getCount()
+        {
+            return Count;
+        }
+        public static int getExpaierdCards()
+        {
+            return ExpaierdCards;
+        }
+        public Card() 
+        {
+            Count++;
+        }
+        public Card(string FIO):this()
+        {
+            clientFIO = FIO;
+
+        }
+        public Card(string FIO, int number):this()
+        {
+            this.number = number;
+            clientFIO = FIO;
+        }
+        public Card(int secureCode, string type = "visa"):this()
+        {
+            this.SecureCode = secureCode;
+            this.Type = type;
+        }
+        
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shkalygin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Card card = new Card();
+            card.Number = 31232132;
+            card.Validity = DateTime.Now;
+            card.Type = "visa";
+            card.ClientFIO = "Шкалыгин Максим";
+            card.SecureCode = 443;
+            Card card1 = new Card("ШКалыгин Максим");
+            card1.Number = 45124123;
+            card1.Validity = DateTime.Now;
+            card1.Type = "мир";
+            card1.SecureCode = 332;
+            Card card2 = new Card("ШКалыгин Максим", 220245453);
+            card2.Type = "mastercard";
+            card2.Number = 78575675;
+            card2.Validity = DateTime.Now;
+            card2.SecureCode = 990;
+            Card card3 = new Card(322);
+            card3.Validity = DateTime.Parse("01.01.2025");
+            card2.ClientFIO = "ШКалыгин Максим";
+            card3.Number = 980797897;
+            card3.SecureCode = 668;
+            Console.WriteLine(card.GetInfo());
+            Console.WriteLine(card1.GetInfo());
+            Console.WriteLine(card2.GetInfo());
+            Console.WriteLine(card3.GetInfo());
+            Console.WriteLine($"Колличество карт {Card.getCount()}");
+            Console.WriteLine($"Колличество заканчивающихся карт {Card.getExpaierdCards()}");
+        }
+
+    }
+}
